18d7b8fe7c5fe2d36fdeb428389d02a5259eaaf8
Arman <akr@agilis-sa.gr>
2020-01-27 19:23:05 +0200

    changes in the level that the include prop is kept + some extra comments

--- sdmx-tck-api/src/model/CubeRegionObject.js
+++ sdmx-tck-api/src/model/CubeRegionObject.js
@@ -28,12 +28,17 @@ class CubeRegionObject {
         for(let i=0;i<KeyValue.length;i++){
            for(let j=0;j<KeyValue[i].Value.length;j++){
             valueArr.push({
-                value:KeyValue[i].Value[j]._ ,
-                includeType:isDefined(KeyValue[i].$) && isDefined(KeyValue[i].$.include)? KeyValue[i].$.include : this.includeType
+                value:KeyValue[i].Value[j]._ 
              });
             }
+            /* Each Key Value contains:
+                a) Its id.
+                b) If it has its own include property we keep it else the Key Value inherits the include type of its cube region.
+                c) An array of its values.
+            */
             keyValueArr.push({
                 id:KeyValue[i].$.id,
+                includeType:isDefined(KeyValue[i].$) && isDefined(KeyValue[i].$.include)? KeyValue[i].$.include : this.includeType,
                 value:valueArr
             })
             valueArr = [];
