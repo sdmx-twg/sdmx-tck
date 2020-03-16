0f2732c28f74e92f5745b297117fd212f26222af
Arman <akr@agilis-sa.gr>
2020-03-16 19:43:16 +0200

    small change in the model of the DataKeySet

--- sdmx-tck-api/src/model/DataKeySetObject.js
+++ sdmx-tck-api/src/model/DataKeySetObject.js
@@ -23,14 +23,26 @@ class DataKeySetObject {
     createKeysStruct(keys){
         let keyValues = [];
         let keyData = [];
+        let includeType;
         for(let i=0;i<keys.length;i++){
            for(let j=0;j<keys[i].KeyValue.length;j++){
               
+                //if there is include type in the KeyValue level
+                if(isDefined(keys[i].KeyValue[j].$) && isDefined(keys[i].KeyValue[j].$.include)){
+                    includeType = keys[i].KeyValue[j].$.include;
+                }else{
+                    //if there is include type in the Key level
+                    if(isDefined(keys[i].$) && isDefined(keys[i].$.include)){
+                        includeType = keys[i].$.include;
+                    }else{
+                        includeType = this.includeType;
+                    }
+                }
                //Create obj of each KeyValue    
                keyValues.push({
                     id:keys[i].KeyValue[j].$.id,
-                    includeType:isDefined(keys[i].KeyValue[j].$) && isDefined(keys[i].KeyValue[j].$.include)? keys[i].KeyValue[j].$.isIncluded : this.includeType,                    
-                    values:keys[i].KeyValue[j].Value[0]._  //Get Value of each KeyValue (each keyValue in DataKeySets has one Value)
+                    includeType:includeType,
+                    value:keys[i].KeyValue[j].Value[0]._  //Get Value of each KeyValue (each keyValue in DataKeySets has one Value)
                });
             }
             keyData.push({keyValues:keyValues})
