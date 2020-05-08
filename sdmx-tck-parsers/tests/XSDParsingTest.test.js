cf4c81a1c3d54acae1eea399562d74fb887bd86a
KRIKORIAN Arman <akr@agilis-sa.gr>
2020-05-08 18:29:31 +0300

    XSD Parsing Tests

---
+++ sdmx-tck-parsers/tests/XSDParsingTest.test.js
@@ -0,0 +1,17 @@
+var SdmxXmlParser = require('../src/parsers/SdmxXmlParser.js');
+const fs = require('fs');
+
+
+describe('Tests XSD Parser', function () {
+    it('It should print a parsed xsd', async () => {
+       
+        let xmlMessage = fs.readFileSync('./tests/resources/XSDexample.xsd','utf8')
+        await new SdmxXmlParser().getIMObjects(xmlMessage).then(function (sdmxObjects) {
+            //console.log(sdmxObjects.structures.get("complexTypes")[0].compositors)
+            //console.log(sdmxObjects.structures.get("complexTypes")[0].compositors[0].compositors[0].elements)
+            console.log(sdmxObjects.structures.get("complexTypes")[0].compositors[0].compositors[0].compositors[0])
+        }).catch(function (err) {
+            console.log(err);
+        });
+    });
+});
