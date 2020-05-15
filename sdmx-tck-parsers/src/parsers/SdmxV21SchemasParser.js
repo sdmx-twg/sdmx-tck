b4421f14eff252a2f352740f233e85317b93d1f1
KRIKORIAN Arman <akr@agilis-sa.gr>
2020-05-15 12:35:49 +0300

    changed the XSD parser mapping keys to constant values

--- sdmx-tck-parsers/src/parsers/SdmxV21SchemasParser.js
+++ sdmx-tck-parsers/src/parsers/SdmxV21SchemasParser.js
@@ -1,6 +1,7 @@
 var XSDGlobalElement = require('sdmx-tck-api').model.XSDGlobalElement;
 var XSDSimpleType = require('sdmx-tck-api').model.XSDSimpleType;
 var XSDComplexType = require('sdmx-tck-api').model.XSDComplexType;
+const XSD_COMPONENTS_TYPES = require('sdmx-tck-api').constants.XSD_COMPONENTS_TYPES;
 var SdmxV21SchemaEnumerationParser = require('./SdmxV21SchemaEnumerationParser.js')
 var SdmxV21SchemaFacetsParser = require('./SdmxV21SchemaFacetsParser.js')
 var SdmxV21SchemaCompositorsParser = require('./SdmxV21SchemaCompositorsParser.js')
@@ -17,27 +18,29 @@ class SdmxV21SchemasParser {
         if (sdmxJsonObjects && sdmxJsonObjects.schema) {
             let s = sdmxJsonObjects.schema;
             //console.log(s)
-            SdmxV21SchemasParser.parseElements(schemaComponents, s);
+            SdmxV21SchemasParser.parseGlobalElements(schemaComponents, s);
             SdmxV21SchemasParser.parseSimpleTypes(schemaComponents, s);
             SdmxV21SchemasParser.parseComplexTypes(schemaComponents, s);
         }
         return schemaComponents;
     };
-    static parseElements(schemaComponents, s) {
+    static parseGlobalElements(schemaComponents, s) {
         if (s.element) {
             let elements = s.element;
-            schemaComponents.set("elements", []);
+            let xsdComponentType = XSD_COMPONENTS_TYPES.GLOBAL_ELEMENT;
+            schemaComponents.set(xsdComponentType, []);
             for (var e in elements) {
-                schemaComponents.get("elements").push(new XSDGlobalElement(elements[e]));
+                schemaComponents.get(xsdComponentType).push(new XSDGlobalElement(elements[e]));
             }
         }
     };
     static parseSimpleTypes(schemaComponents, s) {
         if(s.simpleType){
             let simpleTypes = s.simpleType;
-            schemaComponents.set("simpleTypes",[]);
+            let xsdComponentType = XSD_COMPONENTS_TYPES.SIMPLE_TYPE;
+            schemaComponents.set(xsdComponentType,[]);
             for (var st in simpleTypes){
-                schemaComponents.get("simpleTypes").push(new XSDSimpleType(simpleTypes[st],
+                schemaComponents.get(xsdComponentType).push(new XSDSimpleType(simpleTypes[st],
                                                         SdmxV21SchemaFacetsParser.getFacets(simpleTypes[st]),
                                                         SdmxV21SchemaEnumerationParser.getEnumerations(simpleTypes[st])));
             }
@@ -46,9 +49,10 @@ class SdmxV21SchemasParser {
     static parseComplexTypes(schemaComponents, s) {
         if(s.complexType){
           let complexTypes = s.complexType;
-          schemaComponents.set("complexTypes",[]);
+          let xsdComponentType = XSD_COMPONENTS_TYPES.COMPLEX_TYPE;
+          schemaComponents.set(xsdComponentType,[]);
           for (var ct in complexTypes){
-              schemaComponents.get("complexTypes").push(new XSDComplexType(complexTypes[ct],
+              schemaComponents.get(xsdComponentType).push(new XSDComplexType(complexTypes[ct],
                                                       SdmxV21SchemaCompositorsParser.getCompositors(complexTypes[ct]),
                                                       SdmxV21SchemaAttributeParser.getAttributes(complexTypes[ct])));
           }
