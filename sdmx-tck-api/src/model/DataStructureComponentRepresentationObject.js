545f20ce0d78e0d3c070192b616a4b85aba43253
KRIKORIAN Arman <akr@agilis-sa.gr>
2020-05-27 19:01:49 +0300

    added extra restriction properties in DataStructureComponentRepresentationObject

--- sdmx-tck-api/src/model/DataStructureComponentRepresentationObject.js
+++ sdmx-tck-api/src/model/DataStructureComponentRepresentationObject.js
@@ -3,6 +3,10 @@ class DataStructureComponentRepresentationObject {
         this.type = type,
         this.minLength = props.$.minLength,
         this.maxLength = props.$.maxLength,
+        this.minValue = props.$.minValue
+        this.maxValue = props.$.maxValue,
+        this.decimals = props.$.decimals,
+        this.pattern = props.$.pattern,
         this.textType = props.$.textType
     }
 
@@ -23,6 +27,30 @@ class DataStructureComponentRepresentationObject {
     }
     getMaxLength(){
         return this.maxLength;
+    }
+    setMinValue(minValue){
+        this.minValue = minValue
+    }
+    getMinValue(){
+        return this.minValue
+    }
+    setMaxValue(maxValue){
+        this.maxValue =maxValue
+    }
+    getMaxValue(){
+        return this.maxValue;
+    }
+    setDecimals(decimals){
+        this.decimals = decimals
+    }
+    getDecimals(){
+        return this.decimals;
+    }
+    setPattern(pattern){
+        this.pattern=pattern
+    }
+    getPattern(){
+        return this.pattern;
     }
     setTextType(textType){
         this.textType = textType;
