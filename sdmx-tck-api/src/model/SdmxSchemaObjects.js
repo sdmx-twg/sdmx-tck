f292d91ac317f635445d124ab7f7b5548a5b005e
KRIKORIAN Arman <akr@agilis-sa.gr>
2020-05-27 19:03:06 +0300

    added function in XSD workspace that returns simple types without enums or facets

--- sdmx-tck-api/src/model/SdmxSchemaObjects.js
+++ sdmx-tck-api/src/model/SdmxSchemaObjects.js
@@ -14,13 +14,19 @@ class SdmxSchemaObjects extends SdmxObjects{
     //Returns an array containing the XSD SimpleTypes from workspace, with an enumeration as representation.
     getSimpleTypesWithEnums(){
         return this.getSdmxObjects().get(XSD_COMPONENTS_TYPES.SIMPLE_TYPE).
-                    filter(simpleType=>(Array.isArray(simpleType.getEnumerations()) && simpleType.getEnumerations().length > 0));
+                    filter(simpleType=>(simpleType.getEnumerations().length > 0));
     }
 
     //Returns an array containing the XSD SimpleTypes from workspace, with a facet as representation.
     getSimpleTypesWithFacets(){
         return this.getSdmxObjects().get(XSD_COMPONENTS_TYPES.SIMPLE_TYPE).
-                    filter(simpleType=>(Array.isArray(simpleType.getSchemaFacets()) && simpleType.getSchemaFacets().length>0));
+                    filter(simpleType=>(simpleType.getSchemaFacets().length > 0));
+    }
+
+    //Returns an array containing the XSD SimpleTypes from workspace, without facet or enumerations as representation.
+    getSimpleTypesWithDataTypeRestrictionOnly(){
+        return this.getSdmxObjects().get(XSD_COMPONENTS_TYPES.SIMPLE_TYPE).
+                    filter(simpleType=>(simpleType.getEnumerations().length === 0 && simpleType.getSchemaFacets().length === 0));
     }
 }
 
