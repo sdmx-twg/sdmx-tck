f70513116b3f32352df500543f344adadf5c8053
KRIKORIAN Arman <akr@agilis-sa.gr>
2020-06-03 20:33:15 +0300

    added some more functions in schema workspace

--- sdmx-tck-api/src/model/SdmxSchemaObjects.js
+++ sdmx-tck-api/src/model/SdmxSchemaObjects.js
@@ -6,27 +6,69 @@ class SdmxSchemaObjects extends SdmxObjects{
         super(sdmxObjects)
     }
     
-    //Returns an array containing the XSD components of choice from workspace.
-    getXSDComponentByType(componentType){
-        return this.getSdmxObjects().get(componentType);
+    //Returns an array containing the XSD complex types from workspace.
+    getXSDComplexTypes(){
+        return this.getSdmxObjects().get(XSD_COMPONENTS_TYPES.COMPLEX_TYPE);
+    }
+    //Returns an array containing the XSD global elements from workspace.
+    getXSDGlobalElements(){
+        return this.getSdmxObjects().get(XSD_COMPONENTS_TYPES.GLOBAL_ELEMENT);
+    }
+    //Returns an array containing the XSD simple types from workspace.
+    getXSDSimpleTypes(){
+        return this.getSdmxObjects().get(XSD_COMPONENTS_TYPES.SIMPLE_TYPE);
     }
 
     //Returns an array containing the XSD SimpleTypes from workspace, with an enumeration as representation.
     getSimpleTypesWithEnums(){
-        return this.getSdmxObjects().get(XSD_COMPONENTS_TYPES.SIMPLE_TYPE).
-                    filter(simpleType=>(simpleType.getEnumerations().length > 0));
+        return this.getXSDSimpleTypes().filter(simpleType=>(simpleType.getEnumerations().length > 0));
     }
 
     //Returns an array containing the XSD SimpleTypes from workspace, with a facet as representation.
     getSimpleTypesWithFacets(){
-        return this.getSdmxObjects().get(XSD_COMPONENTS_TYPES.SIMPLE_TYPE).
-                    filter(simpleType=>(simpleType.getSchemaFacets().length > 0));
+        return getXSDSimpleTypes().filter(simpleType=>(simpleType.getSchemaFacets().length > 0));
     }
 
     //Returns an array containing the XSD SimpleTypes from workspace, without facet or enumerations as representation.
     getSimpleTypesWithDataTypeRestrictionOnly(){
-        return this.getSdmxObjects().get(XSD_COMPONENTS_TYPES.SIMPLE_TYPE).
-                    filter(simpleType=>(simpleType.getEnumerations().length === 0 && simpleType.getSchemaFacets().length === 0));
+        return getXSDSimpleTypes().filter(simpleType=>(simpleType.getEnumerations().length === 0 && simpleType.getSchemaFacets().length === 0));
+    }
+    
+    getComplexTypeContainingSpecificAttribute(attributeName){
+        if(!attributeName){
+            throw new Error("Missing mandatory parameter 'attributeName'. ")
+        }
+        let complexType = this.getXSDComplexTypes().filter(function(complexTypeObj) {
+            let someAttribute = complexTypeObj.getAttributeByName(attributeName)
+            if(someAttribute){
+                return someAttribute.getName() === attributeName
+            }
+            return false;
+        })
+        if(complexType.length === 0){
+            return null
+        }
+        return complexType[0];
+
+    }
+    getEnumeratedSimpleTypeOfComponent(componentId){
+       if(!componentId){
+           throw new Error("Missing mandatory parameter 'componentId'. ")
+       }
+        
+        let complexType  = this.getComplexTypeContainingSpecificAttribute(componentId);
+        if(!complexType){
+            return null;
+        }
+        let attribute = complexType.getAttributeByName(componentId);
+        if(!attribute){
+            return null
+        }
+        let chosenSimpleType = this.getSimpleTypesWithEnums().filter(simpleType => simpleType.getName() === attribute.getType())
+        if(chosenSimpleType.length === 0){
+            return null
+        }
+        return chosenSimpleType[0]
     }
 }
 
