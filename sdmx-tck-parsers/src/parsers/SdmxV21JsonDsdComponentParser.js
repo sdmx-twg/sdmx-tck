d0b6d2de812af636a4b7ff9f7d30932f367efe04
KRIKORIAN Arman <akr@agilis-sa.gr>
2020-07-15 18:55:54 +0300

    deleted unnecessary try catch

--- sdmx-tck-parsers/src/parsers/SdmxV21JsonDsdComponentParser.js
+++ sdmx-tck-parsers/src/parsers/SdmxV21JsonDsdComponentParser.js
@@ -19,68 +19,67 @@ class SdmxV21JsonDsdComponentParser {
         let attributes = jsonPath.query(sdmxJsonObject,'$..AttributeList..Attribute')[0]
         let primaryMeasure = jsonPath.query(sdmxJsonObject,'$..MeasureList..PrimaryMeasure')[0]
         let measureDimension = jsonPath.query(sdmxJsonObject,'$..DimensionList..MeasureDimension')[0]
-        try{
-            for(let i in dimensions){
-                let id = (dimensions[i] && dimensions[i].$ && dimensions[i].$.id) ? dimensions[i].$.id : jsonPath.query(dimensions[i].ConceptIdentity[0],'$..Ref')[0][0].$.id
-                //Push in an array the dimension id and the artefact references of the dimension
-                datastructureComponents.push(
-                    new DataStructureComponentObject(
-                        id,
-                        DSD_COMPONENTS_NAMES.DIMENSION,
-                        SdmxV21StructureReferencesParser.getReferences(dimensions[i]),
-                        SdmxV21JsonComponentRepresentationParser.getRepresentation(dimensions[i])))
-                
-            }
-            for(let i in timeDimensions){
-                let id = (timeDimensions[i] && timeDimensions[i].$ && timeDimensions[i].$.id) ? timeDimensions[i].$.id : jsonPath.query(timeDimensions[i].ConceptIdentity[0],'$..Ref')[0][0].$.id
-                //Push in an array the timeDimension id and the artefact references of the timeDimension
-                datastructureComponents.push(
-                    new DataStructureComponentObject(
-                        id,
-                        DSD_COMPONENTS_NAMES.TIME_DIMENSION,
-                        SdmxV21StructureReferencesParser.getReferences(timeDimensions[i]),
-                        SdmxV21JsonComponentRepresentationParser.getRepresentation(timeDimensions[i])))
-                
-                
-            }
-            for(let i in attributes){
-                let id = (attributes[i] && attributes[i].$ && attributes[i].$.id) ? attributes[i].$.id : jsonPath.query(attributes[i].ConceptIdentity[0],'$..Ref')[0][0].$.id
-                //Push in an array the attribute id and the artefact references of the attribute
-                datastructureComponents.push(
-                    new DataStructureAttributeObject(
-                        id,
-                        DSD_COMPONENTS_NAMES.ATTRIBUTE,
-                        SdmxV21StructureReferencesParser.getReferences(attributes[i]),
-                        SdmxV21JsonComponentRepresentationParser.getRepresentation(attributes[i]),
-                        SdmxV21JsonDsdAttributeRelationshipParser.getAttributeRelationship(attributes[i])))
-
-            }
-            for(let i in primaryMeasure){
-                let id = (primaryMeasure[i] && primaryMeasure[i].$ && primaryMeasure[i].$.id) ? primaryMeasure[i].$.id : jsonPath.query(primaryMeasure[i].ConceptIdentity[0],'$..Ref')[0][0].$.id
-                //Push in an array the measure id and the artefact references of the measure
-                datastructureComponents.push(
-                    new DataStructureComponentObject(
-                        id,
-                        DSD_COMPONENTS_NAMES.PRIMARY_MEASURE,
-                        SdmxV21StructureReferencesParser.getReferences(primaryMeasure[i]),
-                        SdmxV21JsonComponentRepresentationParser.getRepresentation(primaryMeasure[i])))
-                
-                
-            }
-            for(let i in measureDimension){
-                let id = (measureDimension[i] && measureDimension[i].$ && measureDimension[i].$.id) ? measureDimension[i].$.id : jsonPath.query(measureDimension[i].ConceptIdentity[0],'$..Ref')[0][0].$.id
-                //Push in an array the measure id and the artefact references of the measure
-                datastructureComponents.push(
-                    new DataStructureComponentObject(
-                        id,
-                        DSD_COMPONENTS_NAMES.MEASURE_DIMENSION,
-                        SdmxV21StructureReferencesParser.getReferences(measureDimension[i]),
-                        SdmxV21JsonComponentRepresentationParser.getRepresentation(measureDimension[i])))
-                
-                
-            }
-        }catch (ex){
+        
+        for(let i in dimensions){
+            let id = (dimensions[i] && dimensions[i].$ && dimensions[i].$.id) ? dimensions[i].$.id : jsonPath.query(dimensions[i].ConceptIdentity[0],'$..Ref')[0][0].$.id
+            //Push in an array the dimension id and the artefact references of the dimension
+            datastructureComponents.push(
+                new DataStructureComponentObject(
+                    id,
+                    DSD_COMPONENTS_NAMES.DIMENSION,
+                    SdmxV21StructureReferencesParser.getReferences(dimensions[i]),
+                    SdmxV21JsonComponentRepresentationParser.getRepresentation(dimensions[i])))
+            
         }
+        for(let i in timeDimensions){
+            let id = (timeDimensions[i] && timeDimensions[i].$ && timeDimensions[i].$.id) ? timeDimensions[i].$.id : jsonPath.query(timeDimensions[i].ConceptIdentity[0],'$..Ref')[0][0].$.id
+            //Push in an array the timeDimension id and the artefact references of the timeDimension
+            datastructureComponents.push(
+                new DataStructureComponentObject(
+                    id,
+                    DSD_COMPONENTS_NAMES.TIME_DIMENSION,
+                    SdmxV21StructureReferencesParser.getReferences(timeDimensions[i]),
+                    SdmxV21JsonComponentRepresentationParser.getRepresentation(timeDimensions[i])))
+            
+            
+        }
+        for(let i in attributes){
+            let id = (attributes[i] && attributes[i].$ && attributes[i].$.id) ? attributes[i].$.id : jsonPath.query(attributes[i].ConceptIdentity[0],'$..Ref')[0][0].$.id
+            //Push in an array the attribute id and the artefact references of the attribute
+            datastructureComponents.push(
+                new DataStructureAttributeObject(
+                    id,
+                    DSD_COMPONENTS_NAMES.ATTRIBUTE,
+                    SdmxV21StructureReferencesParser.getReferences(attributes[i]),
+                    SdmxV21JsonComponentRepresentationParser.getRepresentation(attributes[i]),
+                    SdmxV21JsonDsdAttributeRelationshipParser.getAttributeRelationship(attributes[i])))
+
+        }
+        for(let i in primaryMeasure){
+            let id = (primaryMeasure[i] && primaryMeasure[i].$ && primaryMeasure[i].$.id) ? primaryMeasure[i].$.id : jsonPath.query(primaryMeasure[i].ConceptIdentity[0],'$..Ref')[0][0].$.id
+            //Push in an array the measure id and the artefact references of the measure
+            datastructureComponents.push(
+                new DataStructureComponentObject(
+                    id,
+                    DSD_COMPONENTS_NAMES.PRIMARY_MEASURE,
+                    SdmxV21StructureReferencesParser.getReferences(primaryMeasure[i]),
+                    SdmxV21JsonComponentRepresentationParser.getRepresentation(primaryMeasure[i])))
+            
+            
+        }
+        for(let i in measureDimension){
+            let id = (measureDimension[i] && measureDimension[i].$ && measureDimension[i].$.id) ? measureDimension[i].$.id : jsonPath.query(measureDimension[i].ConceptIdentity[0],'$..Ref')[0][0].$.id
+            //Push in an array the measure id and the artefact references of the measure
+            datastructureComponents.push(
+                new DataStructureComponentObject(
+                    id,
+                    DSD_COMPONENTS_NAMES.MEASURE_DIMENSION,
+                    SdmxV21StructureReferencesParser.getReferences(measureDimension[i]),
+                    SdmxV21JsonComponentRepresentationParser.getRepresentation(measureDimension[i])))
+            
+            
+        }
+        
         return datastructureComponents;
 
     };
