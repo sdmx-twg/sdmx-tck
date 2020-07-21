28bd47c4a5f8ccb4625a9c60ee79eddd8c5f68a2
KRIKORIAN Arman <akr@agilis-sa.gr>
2020-07-21 18:28:20 +0300

    fix for dimensionAtObservation = id test not showing the correct URL

--- sdmx-tck-api/src/constants/SchemaFurtherDescribingParameters.js
+++ sdmx-tck-api/src/constants/SchemaFurtherDescribingParameters.js
@@ -1,5 +1,5 @@
 const SCHEMA_FURTHER_DESCRIBING_PARAMETERS = {
-    AGENCY_ID_VERSION_SPECIFIC_DIM_AT_OBS: { url: "/agency/id/version?dimensionAtObservation=id", template: {dimensionAtObservation:""} },
+    AGENCY_ID_VERSION_SPECIFIC_DIM_AT_OBS: { url: "/agency/id/version?dimensionAtObservation=id", template: {dimensionAtObservation:"id"} },
     AGENCY_ID_VERSION_ALLDIMS: { url: "/agency/id/version?dimensionAtObservation=AllDimensions", template: {dimensionAtObservation:"AllDimensions"} },
     AGENCY_ID_VERSION_EXPLICIT: { url: "/agency/id/version?explicitMeasure=true", template: {explicitMeasure:true} },
    
