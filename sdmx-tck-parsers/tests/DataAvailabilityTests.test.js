a62fa33e9a2adf517eb5b80df15e4c167be6e24d
KRIKORIAN Arman <akr@agilis-sa.gr>
2021-01-19 17:31:20 +0200

    tests for data availability single key queries

--- sdmx-tck-parsers/tests/DataAvailabilityTests.test.js
+++ sdmx-tck-parsers/tests/DataAvailabilityTests.test.js
@@ -20,6 +20,54 @@ describe('Tests Data Availability parent test', function () {
     });
 });
 
+//SINGLE KEY TEST 'mode=exact'
+describe('Tests Data Availability single key (exact) test', function () {
+    it('It should print semantic validation result', async () => {
+                
+        xmlMessage = fs.readFileSync('./tests/resources/DataAvailabilitySimpleKeyExact.xml','utf8')
+        await new SdmxXmlParser().getIMObjects(xmlMessage).then(function (sdmxObjects) {
+            let test = {reqTemplate:{mode:"exact"},randomKeys:[ { CURRENCY: 'RON',
+            EXR_SUFFIX: 'E',
+            CURRENCY_DENOM: 'RON',
+            FREQ: 'D',
+            EXR_TYPE: 'NRU1' },
+          { CURRENCY: 'EGP',
+            EXR_SUFFIX: 'A',
+            CURRENCY_DENOM: 'HUF',
+            FREQ: 'H',
+            EXR_TYPE: 'SP00' } ] }
+            let query={key:"RON.E.RON.D.NRU1"}
+            //let contentconstraint = sdmxObjects.sdmxObjects.get("CONTENT_CONSTRAINT")
+            let result = DataSemanticChecker.checkDataAvailability(test,query,sdmxObjects)
+            console.log(result)
+        }).catch(function (err) {
+            console.log(err);
+        });
+    });
+});
+
+//SINGLE KEY TEST 'mode=available'
+describe('Tests Data Availability single key (available) test', function () {
+    it('It should print semantic validation result', async () => {
+        
+        parentXmlMessage = fs.readFileSync('./tests/resources/DataAvailabilityAll.xml','utf8')
+        let parentObjects = await new SdmxXmlParser().getIMObjects(parentXmlMessage)
+        //parentObjects.sdmxObjects.get('CONTENT_CONSTRAINT') = Object.create(parentObjects.sdmxObjects.get('CONTENT_CONSTRAINT'))
+        console.log(JSON.parse(JSON.stringify(parentObjects)))
+        xmlMessage = fs.readFileSync('./tests/resources/DataAvailabilitySimpleKeyAvailable.xml','utf8')
+        await new SdmxXmlParser().getIMObjects(xmlMessage).then(function (sdmxObjects) {
+            let test = {reqTemplate:{mode:"available"},parentWorkspace:JSON.parse(JSON.stringify(parentObjects))}
+            
+            let query={}
+            //let contentconstraint = sdmxObjects.sdmxObjects.get("CONTENT_CONSTRAINT")
+            let result = DataSemanticChecker.checkDataAvailability(test,query,sdmxObjects)
+            console.log(result)
+        }).catch(function (err) {
+            console.log(err);
+        });
+    });
+});
+
 describe('Tests Data Availability Temporal coverage tests', function () {
     it('It should print semantic validation result', async () => {
                 
