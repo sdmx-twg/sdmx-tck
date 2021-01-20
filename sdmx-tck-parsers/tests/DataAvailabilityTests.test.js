a5e8bdd67e03130fa43c4f4ce330b79679f81450
KRIKORIAN Arman <akr@agilis-sa.gr>
2021-01-20 13:45:47 +0200

    tests for complex keys queries

--- sdmx-tck-parsers/tests/DataAvailabilityTests.test.js
+++ sdmx-tck-parsers/tests/DataAvailabilityTests.test.js
@@ -1,7 +1,7 @@
 var SdmxXmlParser = require('../src/parsers/SdmxXmlParser.js');
 const fs = require('fs');
 const DataSemanticChecker = require('../../sdmx-tck-manager/src/checker/DataSemanticChecker.js');
-var DataRequestPropsBuilder = require('../../sdmx-tck-manager/src/builders/DataRequestPropsBuilder.js')
+const DATA_QUERY_KEY = require('sdmx-tck-api').constants.DATA_QUERY_KEY;
 
 //PARENT TEST 'ALL'
 describe('Tests Data Availability parent test', function () {
@@ -113,6 +113,54 @@ describe('Tests Data Availability Metric test', function () {
         await new SdmxXmlParser().getIMObjects(xmlMessage).then(function (sdmxObjects) {
             let test = {reqTemplate:{}}
             let query={metrics:true}
+            //let contentconstraint = sdmxObjects.sdmxObjects.get("CONTENT_CONSTRAINT")
+            let result = DataSemanticChecker.checkDataAvailability(test,query,sdmxObjects)
+            console.log(result)
+        }).catch(function (err) {
+            console.log(err);
+        });
+    });
+});
+
+//COMPLEX KEY TEST 'mode=exact'
+describe('Tests Data Availability complex key (exact) test', function () {
+    it('It should print semantic validation result', async () => {
+                
+        xmlMessage = fs.readFileSync('./tests/resources/DataAvailabilityComplexKeyExact.xml','utf8')
+        await new SdmxXmlParser().getIMObjects(xmlMessage).then(function (sdmxObjects) {
+            let test = {reqTemplate:{mode:"exact",key:DATA_QUERY_KEY.MANY_KEYS},randomKeys:[ { CURRENCY: 'RON',
+            EXR_SUFFIX: 'E',
+            CURRENCY_DENOM: 'RON',
+            FREQ: 'D',
+            EXR_TYPE: 'NRU1' },
+          { CURRENCY: 'EGP',
+            EXR_SUFFIX: 'A',
+            CURRENCY_DENOM: 'HUF',
+            FREQ: 'H',
+            EXR_TYPE: 'SP00' } ] }
+            let query={key:"RON+EGP.E.RON.D.NRU1"}
+            //let contentconstraint = sdmxObjects.sdmxObjects.get("CONTENT_CONSTRAINT")
+            let result = DataSemanticChecker.checkDataAvailability(test,query,sdmxObjects)
+            console.log(result)
+        }).catch(function (err) {
+            console.log(err);
+        });
+    });
+});
+
+//SINGLE KEY TEST 'mode=available'
+describe('Tests Data Availability complex key (available) test', function () {
+    it('It should print semantic validation result', async () => {
+        
+        parentXmlMessage = fs.readFileSync('./tests/resources/DataAvailabilityAll.xml','utf8')
+        let parentObjects = await new SdmxXmlParser().getIMObjects(parentXmlMessage)
+
+        
+        xmlMessage = fs.readFileSync('./tests/resources/DataAvailabilitySimpleKeyAvailable.xml','utf8')
+        await new SdmxXmlParser().getIMObjects(xmlMessage).then(function (sdmxObjects) {
+            let test = {reqTemplate:{mode:"available",key:DATA_QUERY_KEY.MANY_KEYS},parentWorkspace:JSON.parse(JSON.stringify(parentObjects))}
+            
+            let query={}
             //let contentconstraint = sdmxObjects.sdmxObjects.get("CONTENT_CONSTRAINT")
             let result = DataSemanticChecker.checkDataAvailability(test,query,sdmxObjects)
             console.log(result)
