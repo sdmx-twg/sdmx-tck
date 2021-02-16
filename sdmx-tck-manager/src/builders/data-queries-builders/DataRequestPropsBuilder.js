dc04a07a88a65307ce6e57e381cefa91102c914b
KRIKORIAN Arman <akr@agilis-sa.gr>
2021-02-16 16:53:01 +0200

    small changes in data request properties error handling

--- sdmx-tck-manager/src/builders/data-queries-builders/DataRequestPropsBuilder.js
+++ sdmx-tck-manager/src/builders/data-queries-builders/DataRequestPropsBuilder.js
@@ -1,5 +1,7 @@
 const DATA_QUERY_KEY = require('sdmx-tck-api').constants.DATA_QUERY_KEY;
 const DIMENSION_AT_OBSERVATION_CONSTANTS = require('sdmx-tck-api').constants.DIMENSION_AT_OBSERVATION_CONSTANTS;
+const SeriesObject = require('sdmx-tck-api').model.SeriesObject;
+const ObservationObject = require('sdmx-tck-api').model.ObservationObject;
 class DataRequestPropsBuilder {
     
     static getFlow(testIdentifiers,template){
@@ -16,15 +18,18 @@ class DataRequestPropsBuilder {
         return;
     }
     static getComponent(randomKeys,template){
-        if(!randomKeys){return;}
-        if(template.component){
-            let randIndex = Math.floor(Math.random() * Object.keys(randomKeys[0]).length)
-            return Object.keys(randomKeys[0])[randIndex]
+        if(!template.component){return;}
+        if(!randomKeys || !randomKeys[0]){
+            throw new Error ("Unable to get Dimension.")
         }
-        return;
+        let randIndex = Math.floor(Math.random() * Object.keys(randomKeys[0]).length)
+        return Object.keys(randomKeys[0])[randIndex]
     }
     static getKey(randomKeys,template){
-        if(!randomKeys){return;}
+        if(!template.key || (template.key !== DATA_QUERY_KEY.FULL_KEY && template.key !== DATA_QUERY_KEY.PARTIAL_KEY && template.key !== DATA_QUERY_KEY.MANY_KEYS)){return;}
+        if(!randomKeys || !randomKeys[0]){
+            throw new Error ("Unable to get Key.")
+        }
         let fullKeyValues = Object.values(randomKeys[0])
         
         if(template.key === DATA_QUERY_KEY.FULL_KEY){
@@ -37,6 +42,9 @@ class DataRequestPropsBuilder {
             return fullKeyValuesJoined.replace(fullKeyValues[2],"");
 
         }else if(template.key === DATA_QUERY_KEY.MANY_KEYS){
+            if(!randomKeys[1]){
+                throw new Error ("Unable to get Keys.")
+            }
             let assistiveFullKeyValues = Object.values(randomKeys[1])
             for(let i in fullKeyValues){
                 if(fullKeyValues[i]!== assistiveFullKeyValues[i]){
@@ -46,11 +54,13 @@ class DataRequestPropsBuilder {
             }
             return fullKeyValues.join(".")
         }
-        return;
     }
 
     static getProvider(providerRefs,template){
         if(!template.provider){return;}
+        if(providerRefs.length === 0){
+            throw new Error("Unable to find Providers")
+        }
         if(providerRefs.length === 1){
             if(template.provider && template.provider.providerAgency &&  template.provider.providerId) {
                 return providerRefs[0].agencyId+","+providerRefs[0].identifiableIds[0]
@@ -67,6 +77,12 @@ class DataRequestPropsBuilder {
     static getStartPeriod(indicativeSeries,template){
         if(!template.startPeriod){return;}
         if(typeof template.startPeriod === "string"){return template.startPeriod}
+
+        if(!indicativeSeries instanceof SeriesObject || indicativeSeries.getObservations().length === 0
+            || indicativeSeries.getObservations().some(obs=>!obs instanceof ObservationObject)){
+
+            throw new Error ("Unable to get a starting period.")
+        }
         let observations = indicativeSeries.getObservations()
         if(observations.length ===1 || observations.length ===2 || observations.length ===3){
             return observations[0].getAttributes().TIME_PERIOD;
@@ -78,6 +94,11 @@ class DataRequestPropsBuilder {
     static getEndPeriod(indicativeSeries,template){
         if(!template.endPeriod){return;}
         if(typeof template.endPeriod === "string"){return template.endPeriod}
+        if(!indicativeSeries instanceof SeriesObject || indicativeSeries.getObservations().length === 0
+            || indicativeSeries.getObservations().some(obs=>!obs instanceof ObservationObject)){
+
+            throw new Error ("Unable to get an ending period.")
+        }
         let observations = indicativeSeries.getObservations()
         if(observations.length ===1){
             return observations[0].getAttributes().TIME_PERIOD;
@@ -88,7 +109,12 @@ class DataRequestPropsBuilder {
         }
     }
 
-    static getNumberOfObs(indicativeSeries,template){
+    static getNumberOfObs(indicativeSeries){
+        if(!indicativeSeries instanceof SeriesObject || indicativeSeries.getObservations().length === 0
+            || indicativeSeries.getObservations().some(obs=>!obs instanceof ObservationObject)){
+
+            throw new Error ("Unable to get a number of observations.")
+        }
         let observations = indicativeSeries.getObservations()
         if(observations.length ===1 || observations.length ===2 ){
             return observations.length;
@@ -101,18 +127,25 @@ class DataRequestPropsBuilder {
 
     static getNumOfFirstNObservations(indicativeSeries,template){
         if(!template.firstNObservations){return;}
-        return this.getNumberOfObs(indicativeSeries,template)
+        return this.getNumberOfObs(indicativeSeries)
     }
 
     static getNumOfLastNObservations(indicativeSeries,template){
         if(!template.lastNObservations){return;}
+        if(!indicativeSeries instanceof SeriesObject){
+            throw new Error ("Unable to get a number of observations.")
+        }
         if(template.startPeriod && template.endPeriod && template.lastNObservations){
             indicativeSeries.setObservations(indicativeSeries.getObservationsBetweenPeriod(this.getStartPeriod(indicativeSeries,template),this.getEndPeriod(indicativeSeries,template)))
         }
-        return this.getNumberOfObs(indicativeSeries,template)
+        return this.getNumberOfObs(indicativeSeries)
     }
     static getUpdateAfterDate(indicativeSeries,template){
         if(!template.updateAfter){return;}
+        if(!indicativeSeries instanceof SeriesObject || indicativeSeries.getObservations().length === 0
+            || indicativeSeries.getObservations().some(obs=>!obs instanceof ObservationObject)){
+            throw new Error ("Unable to get a date.")
+        }
         let observations = indicativeSeries.getObservations();
         let randomIndex = Math.floor(Math.random() * observations.length);
         return observations[randomIndex].getAttributes().TIME_PERIOD;
