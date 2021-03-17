e8340487625beb3ae66c3dc077aa97db6fc4ea13
KRIKORIAN Arman <akr@agilis-sa.gr>
2021-03-17 13:41:54 +0200

    small variable name change in dataIdentificationParameters.js

--- sdmx-tck-api/src/constants/data-queries-constants/DataIdentificationParameters.js
+++ sdmx-tck-api/src/constants/data-queries-constants/DataIdentificationParameters.js
@@ -12,17 +12,17 @@ const DATA_IDENTIFICATION_PARAMETERS = {
     AGENCY_ID_VERSION_ALL_AGENCY_PRVIDERID1_PROVIDERID2:{ url:"/agency,id,version/all/providerId1+providerId2", template: {provider:{num:2,providerId:true},detail:DATA_QUERY_DETAIL.SERIES_KEYS_ONLY}},
 
     getDataIdentificationParameters(apiVersion) {
-            var availableDetailValues = [];
-            availableDetailValues.push(DATA_IDENTIFICATION_PARAMETERS.AGENCY_ID_VERSION);
-            availableDetailValues.push(DATA_IDENTIFICATION_PARAMETERS.AGENCY_ID);
-            availableDetailValues.push(DATA_IDENTIFICATION_PARAMETERS.ID);
-            availableDetailValues.push(DATA_IDENTIFICATION_PARAMETERS.AGENCY_ID_VERSION_ALL_AGENCY_PROVIDERID)
-            availableDetailValues.push(DATA_IDENTIFICATION_PARAMETERS.AGENCY_ID_VERSION_ALL_PROVIDERID)
+            var availableTests= [];
+            availableTests.push(DATA_IDENTIFICATION_PARAMETERS.AGENCY_ID_VERSION);
+            availableTests.push(DATA_IDENTIFICATION_PARAMETERS.AGENCY_ID);
+            availableTests.push(DATA_IDENTIFICATION_PARAMETERS.ID);
+            availableTests.push(DATA_IDENTIFICATION_PARAMETERS.AGENCY_ID_VERSION_ALL_AGENCY_PROVIDERID)
+            availableTests.push(DATA_IDENTIFICATION_PARAMETERS.AGENCY_ID_VERSION_ALL_PROVIDERID)
             if (API_VERSIONS[apiVersion] >= API_VERSIONS["v1.3.0"]) {
-                availableDetailValues.push(DATA_IDENTIFICATION_PARAMETERS.AGENCY_ID_VERSION_ALL_AGENCY_PRVIDERID1_PROVIDERID2)
+                availableTests.push(DATA_IDENTIFICATION_PARAMETERS.AGENCY_ID_VERSION_ALL_AGENCY_PRVIDERID1_PROVIDERID2)
                 
             }
-            return availableDetailValues;
+            return availableTests;
         }
     };
 
